b3e0eea959cdaffdbd8c20aa42b5082fb9582509
Alex Elkin <alelkdev@gmail.com>
2024-06-23 22:55:30 +0300

    feat: improve console app; add readme

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,11 +1,17 @@
+const val createCmd = "create"
+const val addCmd = "add"
+const val removeCmd = "remove"
+
+fun String.parseIntList(): List<Int> = split(',').mapNotNull { it.trim().toIntOrNull() }
+
 fun main() {
 
     val helpText = """
         | Справка:
         |   help - вывод справки
-        |   create [el1], [el2], ... - создать новое дерево
-        |   add - добавить элементы
-        |   remove - удалить элемент
+        |   $createCmd [el1], [el2], ... - создать новое дерево
+        |   $addCmd [el1], [el2], ... - добавить элементы
+        |   $removeCmd [el1], [el2], ... - удалить элементы
         |   exit - выйти из программы
         |""".trimMargin()
 
@@ -17,26 +23,50 @@ fun main() {
             textBox.text.forEach { l ->
                 println(l)
             }
-        }
+        } else println("Дерево не содержит элементов")
     }
 
     println(helpText)
 
     while (true) {
         println("Введите команду")
-        val cmd = readlnOrNull()
+        val cmd = readlnOrNull()?.trimStart()
         if (cmd == null) {
             System.err.println("Вы не ввели команду!!!")
             continue
         }
-        if (cmd.trim().startsWith("create")) {
-            val numbers = cmd.replace("create", "").split(',').mapNotNull { it.trim().toIntOrNull() }
-            if (numbers.isEmpty()) {
-                System.err.println("Введите хотя бы одно число!!!")
-                continue
+        when {
+            cmd.trim() == "help" -> println(helpText)
+            cmd.trim() == "exit" -> break
+            cmd.startsWith(createCmd) -> {
+                val numbers = cmd.drop(createCmd.length).parseIntList()
+                if (numbers.isEmpty()) {
+                    System.err.println("Введите хотя бы одно число!!!")
+                    continue
+                }
+                tree = numbers.fold(null as Tree?) { acc, e -> acc?.add(e) ?: Tree(e) }
+                printTree()
             }
-            tree = numbers.fold(null as Tree?) { acc, e -> acc?.add(e) ?: Tree(e) }
-            printTree()
+
+            cmd.startsWith(addCmd) -> {
+                val numbers = cmd.drop(addCmd.length).parseIntList()
+                if (numbers.isEmpty()) {
+                    System.err.println("Введите хотя бы одно число!!!")
+                    continue
+                }
+                tree = numbers.fold(tree) { acc, e -> acc?.add(e) ?: Tree(e) }
+                printTree()
+            }
+
+            cmd.startsWith(removeCmd) -> {
+                val numbers = cmd.drop(removeCmd.length).parseIntList()
+                if (numbers.isEmpty()) {
+                    System.err.println("Введите хотя бы одно число!!!")
+                    continue
+                }
+                tree = numbers.fold(tree) { acc, e -> acc?.remove(e) }
+                printTree()
+            }
         }
     }
 }
