0e0fa0fef3c441a7924e79bfa7aa89c5e09014e4
Alex Elkin <alelkdev@gmail.com>
2024-09-03 19:12:15 +0300

    small fix

--- src/main/kotlin/Tree.kt
+++ src/main/kotlin/Tree.kt
@@ -9,20 +9,15 @@ data class Tree(val data: Int, val left: Tree? = null, val right: Tree? = null) 
     val isLeaf: Boolean = left == null && right == null
 
     /** Добавить заданное значение в дерево. Возвращает копию текущего дерева */
-    fun add(newData: Int): Tree =
-        if (newData == data)
-            this
-        else if (newData < data)
-            this.copy(left = left?.add(newData) ?: Tree(newData))
-        else
-            this.copy(right = right?.add(newData) ?: Tree(newData))
+    fun add(data: Int): Tree =
+        if (data == this.data) this
+        else if (data < this.data) this.copy(left = left?.add(data) ?: Tree(data))
+        else this.copy(right = right?.add(data) ?: Tree(data))
 
     /** Удаляет заданное значение из дерева. Возвращает копию текущего дерева */
-    fun remove(dataToRemove: Int): Tree? =
-        if (dataToRemove < data)
-            this.copy(left = left?.remove(dataToRemove))
-        else if (dataToRemove > data)
-            this.copy(right = right?.remove(dataToRemove))
+    fun remove(data: Int): Tree? =
+        if (data < this.data) this.copy(left = left?.remove(data))
+        else if (data > this.data) this.copy(right = right?.remove(data))
         else if (isLeaf) null
         else if (left == null) right
         else right?.joinLeft(left) ?: left
@@ -30,4 +25,6 @@ data class Tree(val data: Int, val left: Tree? = null, val right: Tree? = null) 
     private fun joinLeft(t: Tree): Tree =
         this.copy(left = left?.joinLeft(t) ?: t)
 
+    operator fun plus(data: Int): Tree = add(data)
+    operator fun minus(data: Int): Tree? = remove(data)
 }
